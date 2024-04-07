c3901c3acc5a456b123a3827507c61b3b927e8dc
c5huracan <c5huracan@protonmail.ch>
2024-04-07 06:38:16 -0500

    updates to layers.py

--- layers.py
+++ layers.py
@@ -25,3 +25,26 @@ class Linear(Layer):
     """
     computes output = inputs @ w + b
     """
+
+    def __init__(self, input_size: int, output_size: int) -> None:
+        # inputs will be (batch_size, input_size)
+        # outputs will be (batch_size, output_size)
+        super().__init__()
+        self.params["w"] = np.random.randn(input_size, output_size)
+        self.params["b"] = np.random.randn(output_size)
+
+    def forward(self, inputs: Tensor) -> Tensor:
+        """
+        outputs = inputs @ w +b
+        """
+        self.inputs = inputs
+        return inputs @ self.params["w"] + self.params["b"]
+
+    def backward(self, grad: Tensor) -> Tensor:
+        """
+        if y = f(x) and x = a * b + c
+        then dy/da = f'(x) @ b.T
+        """
+        self.grads["w"] = self.inputs.T @ grad
+        self.grads["b"] = np.sum(grad, axis=0)
+        return grad @ self.params["w"].T
