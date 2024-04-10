1e8c404cce5cd30de755443dba64bb26e3e32449
c5huracan <c5huracan@protonmail.ch>
2024-04-09 20:57:50 -0500

    adds nn.py

---
+++ nn.py
@@ -0,0 +1,6 @@
+"""
+A neural network is just a collection of layers.
+I behaves a lot like a layer itself. Although
+we are not going to make it one. 
+"""
+from typing import Sequence, Iterator, Tuple
